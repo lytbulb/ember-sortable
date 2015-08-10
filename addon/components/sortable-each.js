4d2d59379e3aee15eff3cd9af44aac672ea6c213
denzo <deniss.alimovs@gmail.com>
2015-08-10 10:46:08 +1000

    implement sortable-each

---
+++ addon/components/sortable-each.js
@@ -0,0 +1,8 @@
+import SortableGroup from '../components/sortable-group';
+import layout from '../templates/components/sortable-each';
+
+export default SortableGroup.extend({
+
+	layout: layout
+	
+});
---
+++ app/components/sortable-each.js
@@ -0,0 +1,3 @@
+import sortableEach from 'ember-sortable/components/sortable-each';
+
+export default sortableEach;
